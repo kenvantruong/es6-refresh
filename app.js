4235f2f5fb778bd22cea82c2e633fffe9935beb1
Ken Truong <kenvantruong@gmail.com>
2018-07-26 11:52:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Array Helper Methods
+/* forEach | map | filter | find | every | some | reduce */
+
+
+// forEach
+var number = ['jordan', 'kobe', 'dwayne', 'lebron']
+
+number.forEach(function(e, i){
+  console.log(number);
+})
+
